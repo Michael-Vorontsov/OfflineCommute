5aeabfc0d49a4570b573884f60d9a33eb47143e4
Mykhailo Vorontsov <mykhailo.vorontsov@lebara.com>
2016-06-27 14:37:07 +0100

    Use catchable open street map tiles.

--- OfflineCommute/Sources/UI/Scenes/StationsListViewController.swift
+++ OfflineCommute/Sources/UI/Scenes/StationsListViewController.swift
@@ -51,6 +51,7 @@ class StationsListViewController: LocalizableViewController, NSFetchedResultsCon
   lazy var mapView:MKMapView = {
     let mapView = MKMapView(frame: self.mapViewContainer.bounds)
     mapView.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+    mapView.delegate = self
     return mapView
     
   }()
@@ -101,6 +102,59 @@ class StationsListViewController: LocalizableViewController, NSFetchedResultsCon
   }()
 }
 
+
+class VHTileOverlay:MKTileOverlay {
+  
+  let operationQueue = NSOperationQueue()
+  let directoryPath = NSFileManager.applicationCachesDirectory
+  let session = NSURLSession.sharedSession()
+
+  override init(URLTemplate: String?) {
+    super.init(URLTemplate: URLTemplate)
+  }
+  
+//  override func URLForTilePath(path: MKTileOverlayPath) -> NSURL {
+//    return NSURL(string: NSString(format: "http://tile.openstreetmap.org/%ld/%ld/%ld.png", path.z, path.x, path.y) as String)!
+//  }
+  
+  override func loadTileAtPath(path: MKTileOverlayPath, result: (NSData?, NSError?) -> Void) {
+//    guard let result = result else {
+//      return
+//    }
+    
+    var pathToFile = self.URLForTilePath(path).absoluteString
+    pathToFile = pathToFile.stringByReplacingOccurrencesOfString("/", withString: "|")
+    if let cachedData = self.loadFileWithName(pathToFile as String) {
+      result(cachedData, nil)
+    }
+    
+    let task = session.dataTaskWithURL(self.URLForTilePath(path)) { (data, response, error) in
+      if let data = data {
+        self.saveFileWithName(pathToFile, imageData: data)
+      }
+      result(data, error)
+    }
+    task.resume()
+    
+  }
+  
+  func pathToImage(imageName:String) -> NSURL {
+    let imageFile = directoryPath.URLByAppendingPathComponent(imageName)
+    return imageFile
+  }
+
+  func loadFileWithName(fileName:String) -> NSData? {
+    let data = NSData(contentsOfURL: pathToImage(fileName))
+    return data
+  }
+  
+  func saveFileWithName(name:String, imageData:NSData) -> Bool {
+    let success = imageData.writeToURL(pathToImage(name), atomically: true)
+    return success
+  }
+  
+}
+
 // MARK: -Overrides
 
 extension StationsListViewController {
@@ -110,8 +164,21 @@ extension StationsListViewController {
 
     
     super.viewDidLoad()
+
     
-    mapViewContainer.addSubview(mapView)
+    let template = "http://tile.openstreetmap.org/{z}/{x}/{y}.png";
+    let overlay = VHTileOverlay(URLTemplate: template)
+    overlay.canReplaceMapContent = true
+    let newMapView = self.mapView
+    newMapView.addOverlay(overlay, level: .AboveLabels)
+    mapViewContainer.addSubview(newMapView)
+//http://server/path?x={x}&y={y}&z={z}&scale={scale}.
+//    let template = "http://tile.openstreetmap.org/{z}/{x}/{y}.png";
+    
+//    VHTileOverlay *overlay = [[VHTileOverlay alloc] initWithURLTemplate:template];
+//    overlay.canReplaceMapContent = YES;
+//    [self.mapView addOverlay:overlay level:MKOverlayLevelAboveLabels];
+    
     
     
   }
@@ -271,7 +338,13 @@ extension StationsListViewController {
 }
 
 // MARK: - MKMapViewDelegate
-extension StationsListViewController {
+extension StationsListViewController: MKMapViewDelegate {
+  
+  func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+    let render = MKTileOverlayRenderer(overlay: overlay)
+    return render
+  }
+  
 //  func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
 //    guard let annotation = annotation as? DockStation else {
 //      return nil
